12fef1cdb9caf1bc4a0b480ce99737abe9da4047
Santiago Lezica <salezica@gmail.com>
2025-12-25 14:36:25 -0300

    main: load pdfjs from cdn

--- src/main.js
+++ src/main.js
@@ -1,20 +1,27 @@
+import * as pdfjs from "https://cdn.jsdelivr.net/npm/pdfjs-dist@5.4.449/build/pdf.mjs";
+
+pdfjs.GlobalWorkerOptions.workerSrc =
+  "https://cdn.jsdelivr.net/npm/pdfjs-dist@5.4.449/build/pdf.worker.min.mjs";
+
+
 const dropZone = document.getElementById('drop-zone')
 const fileInput = document.getElementById('file-input')
+
 
 dropZone.addEventListener('click', () => {
   fileInput.click()
 })
 
-dropZone.addEventListener('dragover', (e) => {
-  e.preventDefault()
+dropZone.addEventListener('dragover', ev => {
+  ev.preventDefault()
   dropZone.classList.add('drag-over')
 })
 
-dropZone.addEventListener('dragleave', () => {
+dropZone.addEventListener('dragleave', ev => {
   dropZone.classList.remove('drag-over')
 })
 
-dropZone.addEventListener('drop', (e) => {
+dropZone.addEventListener('drop', ev => {
   e.preventDefault()
   dropZone.classList.remove('drag-over')
 
@@ -24,7 +31,7 @@ dropZone.addEventListener('drop', (e) => {
   }
 })
 
-fileInput.addEventListener('change', (e) => {
+fileInput.addEventListener('change', ev => {
   if (e.target.files.length > 0) {
     handleFile(e.target.files[0])
   }
